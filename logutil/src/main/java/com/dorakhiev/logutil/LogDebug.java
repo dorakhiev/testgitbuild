f1bd58ac791f43600158509a883237d00ea77cd3
Dora Khiev <dkhiev@youmail.com>
2020-06-16 02:00:11 -0700

    Added logutil module.

---
+++ logutil/src/main/java/com/dorakhiev/logutil/LogDebug.java
@@ -0,0 +1,11 @@
+package com.dorakhiev.logutil;
+
+import android.util.Log;
+
+public class LogDebug {
+
+    public static void debug(String tag, String message) {
+        Log.d(tag, message);
+    }
+
+}
